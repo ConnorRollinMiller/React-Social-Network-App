cf0b3f75025179465e8f032c3ec0b367b4a1c090
ConnorMiller21 <miller.connor95@gmail.com>
2019-06-30 01:18:24 -0500

    make it better

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-   res.send('API running');
-});
-
 // Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
